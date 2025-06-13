7ca2a384a21a4d0cb0a6b7f23f6fb3a8505314cc
nerokome <nerooghenekome@gmail.com>
2025-06-13 16:53:06 +0100

    put

--- my-app/src/components/TowerSelector.jsx
+++ my-app/src/components/TowerSelector.jsx
@@ -11,7 +11,7 @@ const TowerSelector = ({ towers, onSelect }) => {
       {towers.map((tower, index) => (
         <motion.div
           key={tower}
-          className="p-6 w-40 text-center bg-blue-100 hover:bg-blue-300 rounded-lg shadow-xl cursor-pointer"
+          className="p-6 w-40 text-center bg-blue-100 hover:bg-blue-400 rounded-lg shadow-xl cursor-pointer"
           onClick={() => onSelect(tower)}
           whileHover={{ scale: 1.08 }}
           whileTap={{ scale: 0.95 }}
